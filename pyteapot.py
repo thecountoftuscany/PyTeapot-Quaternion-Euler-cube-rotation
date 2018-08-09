6533138567fd78ed546c7bf60613dfec7fe29005
thecountoftuscany <14308382+thecountoftuscany@users.noreply.github.com>
2018-08-09 14:24:22 +0530

    PyTeapot file
    Uploaded first version of code.

---
+++ pyteapot.py
@@ -0,0 +1,188 @@
+"""
+Nishant's PyTeapot module for drawing rotating cube using OpenGL as per
+quaternion or yaw, pitch, roll angles received over serial port.
+"""
+
+import pygame
+import serial
+import math
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from pygame.locals import *
+
+ser = serial.Serial('/dev/ttyUSB0', 38400)
+useQuat = True   # set true for using quaternions, false for using y,p,r angles
+
+def main():
+    video_flags = OPENGL | DOUBLEBUF
+    pygame.init()
+    screen = pygame.display.set_mode((640, 480), video_flags)
+    pygame.display.set_caption("PyTeapot testing")
+    resizewin(640, 480)
+    init()
+    frames = 0
+    ticks = pygame.time.get_ticks()
+    while 1:
+        event = pygame.event.poll()
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            break
+        if(useQuat):
+            [w, nx, ny, nz] = read_data()
+        else:
+            [yaw, pitch, roll] = read_data()
+        if(useQuat):
+            draw(w, nx, ny, nz)
+        else:
+            draw(1, yaw, pitch, roll)
+        pygame.display.flip()
+        frames += 1
+    print("fps: %d" % ((frames*1000)/(pygame.time.get_ticks()-ticks)))
+    ser.close()
+
+
+def resizewin(width, height):
+    """
+    For resizing window
+    """
+    if height == 0:
+        height = 1
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, 1.0*width/height, 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def init():
+    glShadeModel(GL_SMOOTH)
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LEQUAL)
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+
+
+def cleanSerialBegin():
+    if(useQuat):
+        try:
+            line = ser.readline().decode('UTF-8').replace('\n', '')
+            w = float(line.split('w')[1])
+            nx = float(line.split('a')[1])
+            ny = float(line.split('b')[1])
+            nz = float(line.split('c')[1])
+        except Exception:
+            pass
+    else:
+        try:
+            line = ser.readline().decode('UTF-8').replace('\n', '')
+            yaw = float(line.split('y')[1])
+            pitch = float(line.split('p')[1])
+            roll = float(line.split('r')[1])
+        except Exception:
+            pass
+
+
+def read_data():
+    if(useQuat):
+        ser.reset_input_buffer()
+        cleanSerialBegin()
+        line = ser.readline().decode('UTF-8').replace('\n', '')
+        print(line)
+        w = float(line.split('w')[1])
+        nx = float(line.split('a')[1])
+        ny = float(line.split('b')[1])
+        nz = float(line.split('c')[1])
+        return [w, nx, ny, nz]
+    else:
+        ser.reset_input_buffer()
+        cleanSerialBegin()
+        line = ser.readline().decode('UTF-8').replace('\n', '')
+        print(line)
+        yaw = float(line.split('y')[1])
+        pitch = float(line.split('p')[1])
+        roll = float(line.split('r')[1])
+        return [yaw, pitch, roll]
+
+
+def draw(w, nx, ny, nz):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    glTranslatef(0, 0.0, -7.0)
+
+    drawText((-2.6, 1.8, 2), "PyTeapot", 18)
+    drawText((-2.6, 1.6, 2), "Module to visualize quaternion or Euler angles data", 16)
+    drawText((-2.6, -2, 2), "Press Escape to exit.", 16)
+
+    if(useQuat):
+        [yaw, pitch , roll] = quat_to_ypr([w, nx, ny, nz])
+        drawText((-2.6, -1.8, 2), "Yaw: %f, Pitch: %f, Roll: %f" %(yaw, pitch, roll), 16)
+        glRotatef(2 * math.acos(w) * 180.00/math.pi, -1 * nx, nz, ny)
+    else:
+        yaw = nx
+        pitch = ny
+        roll = nz
+        drawText((-2.6, -1.8, 2), "Yaw: %f, Pitch: %f, Roll: %f" %(yaw, pitch, roll), 16)
+        glRotatef(-roll, 0.00, 0.00, 1.00)
+        glRotatef(pitch, 1.00, 0.00, 0.00)
+        glRotatef(yaw, 0.00, 1.00, 0.00)
+
+    glBegin(GL_QUADS)
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(1.0, 0.2, -1.0)
+    glVertex3f(-1.0, 0.2, -1.0)
+    glVertex3f(-1.0, 0.2, 1.0)
+    glVertex3f(1.0, 0.2, 1.0)
+
+    glColor3f(1.0, 0.5, 0.0)
+    glVertex3f(1.0, -0.2, 1.0)
+    glVertex3f(-1.0, -0.2, 1.0)
+    glVertex3f(-1.0, -0.2, -1.0)
+    glVertex3f(1.0, -0.2, -1.0)
+
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(1.0, 0.2, 1.0)
+    glVertex3f(-1.0, 0.2, 1.0)
+    glVertex3f(-1.0, -0.2, 1.0)
+    glVertex3f(1.0, -0.2, 1.0)
+
+    glColor3f(1.0, 1.0, 0.0)
+    glVertex3f(1.0, -0.2, -1.0)
+    glVertex3f(-1.0, -0.2, -1.0)
+    glVertex3f(-1.0, 0.2, -1.0)
+    glVertex3f(1.0, 0.2, -1.0)
+
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(-1.0, 0.2, 1.0)
+    glVertex3f(-1.0, 0.2, -1.0)
+    glVertex3f(-1.0, -0.2, -1.0)
+    glVertex3f(-1.0, -0.2, 1.0)
+
+    glColor3f(1.0, 0.0, 1.0)
+    glVertex3f(1.0, 0.2, -1.0)
+    glVertex3f(1.0, 0.2, 1.0)
+    glVertex3f(1.0, -0.2, 1.0)
+    glVertex3f(1.0, -0.2, -1.0)
+    glEnd()
+
+
+def drawText(position, textString, size):
+    font = pygame.font.SysFont("Courier", size, True)
+    textSurface = font.render(textString, True, (255, 255, 255, 255), (0, 0, 0, 255))
+    textData = pygame.image.tostring(textSurface, "RGBA", True)
+    glRasterPos3d(*position)
+    glDrawPixels(textSurface.get_width(), textSurface.get_height(), GL_RGBA, GL_UNSIGNED_BYTE, textData)
+
+def quat_to_ypr(q):
+    yaw   = math.atan2(2.0 * (q[1] * q[2] + q[0] * q[3]), q[0] * q[0] + q[1] * q[1] - q[2] * q[2] - q[3] * q[3])
+    pitch = -math.sin(2.0 * (q[1] * q[3] - q[0] * q[2]))
+    roll  = math.atan2(2.0 * (q[0] * q[1] + q[2] * q[3]), q[0] * q[0] - q[1] * q[1] - q[2] * q[2] + q[3] * q[3])
+    pitch *= 180.0 / math.pi
+    yaw   *= 180.0 / math.pi
+    yaw   -= -0.13  # Declination at Chandrapur, Maharashtra is - 0 degress 13 min
+    roll  *= 180.0 / math.pi
+    return [yaw, pitch, roll]
+
+
+if __name__ == '__main__':
+    main()
